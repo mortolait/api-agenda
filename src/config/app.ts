9c81dab6333db2b1a5471de733ca2c52f6fcfe27
mortolait <contatomortola@outlook.com>
2023-04-21 00:49:47 -0300

    ajuste vercel

--- src/config/app.ts
+++ src/config/app.ts
@@ -34,15 +34,15 @@ class App{
         this.app.get('/teste',(req:Request, res:Response)=>{
             res.send("Hello")
         })
-        // this.app.use('/clientSaas',routerClientSaas)
-        // this.app.use('/users',routerUser)
+         this.app.use('/clientSaas',routerClientSaas)
+        //this.app.use('/users',routerUser)
 
         //  this.app.use(auth.validate)
 
         // this.app.use('/clients',routerClient)
         // this.app.use('/professionals',routerProfessional)
         // this.app.use('/states', routerStates)
-        this.app.use('/appointments',routerAppointment)
+        // this.app.use('/appointments',routerAppointment)
         // this.app.use('/typeServices',routerTypeServices)
     }
 
