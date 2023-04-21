a53daa806948bed2b6a668240c401aebbd51ee4e
mortolait <contatomortola@outlook.com>
2023-04-21 00:18:59 -0300

    ajuste vercel

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import { Request, Response } from 'express'
 import server from './config/app'
 import 'dotenv/config'
 
 
 const port = process.env.PORT
-
+server.app.get('/teste',(req:Request,res:Response)=>{
+    res.send("Hello")
+})
 server.app.listen(port,()=>{
     console.log("Server is running")
 })
