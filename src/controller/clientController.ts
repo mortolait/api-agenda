4a9a8a68b93edd33a98d688179f8bd8e56c3be1d
mortolait <contatomortola@outlook.com>
2023-04-21 10:14:23 -0300

    ajuste vercel teste

--- src/controller/clientController.ts
+++ src/controller/clientController.ts
@@ -10,8 +10,8 @@ export class ClientController {
       // const token: any = get(req);
       // const decodedToken: any = jwt.verify(token, "linus-secret");
       // const clients = await Client.find({ clientSaas: decodedToken?.result.clientSaas });
-      const clients = await Client.find({});
-      res.json(clients);
+      // const clients = await Client.find({});
+      res.json({clientes:"teste"}).status(200);
     } catch (error) {
       res.status(500).send(error);
     }
